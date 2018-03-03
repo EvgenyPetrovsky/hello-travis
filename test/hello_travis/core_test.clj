90f0dca31ef2329f19f0c37ed41f3f37a0f82ff4
Functional Programming <ev.petrovsky@gmail.com>
2018-03-03 12:05:19 +0100

    remove failing test

--- test/hello_travis/core_test.clj
+++ test/hello_travis/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [hello-travis.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 (deftest b-test
   (testing "LOOK, I work!"
     (is (= 1 1))))
